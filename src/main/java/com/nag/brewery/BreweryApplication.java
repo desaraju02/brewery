d508327dc075eeb48c7f57169802b7a3f0418747
Desaraju <desaraju02@gmail.com>
2020-12-28 12:22:00 +0530

    Initialization to Git

---
+++ src/main/java/com/nag/brewery/BreweryApplication.java
@@ -0,0 +1,13 @@
+package com.nag.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryApplication.class, args);
+    }
+
+}
